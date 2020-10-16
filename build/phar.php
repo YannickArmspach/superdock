2ca3678ea9ef69eb2db107a3b77eb7a6d62a2761
YannickArmspach <yannick.armspach@gmail.com>
2020-10-16 11:14:17 +0200

    rebuild phar

--- build/phar.php
+++ build/phar.php
@@ -4,4 +4,4 @@ $phar = new Phar( $buildRoot . '/dist/superdock.phar', 0, 'superdock.phar' );
 $include = '/^(?=(.*Command|.*Service|.*bin|.*inc|.*vendor))(.*)$/i';
 $phar->buildFromDirectory($buildRoot, $include);
 $phar->setStub("#!/usr/bin/env php\n" . $phar->createDefaultStub("bin/superdock.php"));
-echo 'build success' . PHP_EOL;
+echo 'build success' . PHP_EOL . PHP_EOL;
