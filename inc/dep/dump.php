41252e268f9f107afee0347825614edd84e57bfe
YannickArmspach <yannick.armspach@gmail.com>
2021-02-11 17:19:49 +0100

    improve dump command

--- inc/dep/dump.php
+++ inc/dep/dump.php
@@ -8,11 +8,7 @@ require 'hosts.php';
 require 'settings.php';
 
 task('dump', function () {
-    if ( get('deploy_db_host') == 'localhost' ) {
-        run('mysqldump --host={{deploy_db_host}} --user={{deploy_db_user}} --password={{deploy_db_pass}} {{deploy_db_name}} > {{deploy_path}}/{{deploy_env}}.sql');
-        download('{{deploy_path}}/{{deploy_env}}.sql', $_ENV['SUPERDOCK_PROJECT_DIR'] . '/superdock/database/{{deploy_env}}/dump.sql');
-        run('rm {{deploy_path}}/{{deploy_env}}.sql');
-    } else {
-        runLocally('mysqldump --host={{deploy_db_host}} --user={{deploy_db_user}} --password={{deploy_db_pass}} {{deploy_db_name}} > ' . $_ENV['SUPERDOCK_PROJECT_DIR'] . '/superdock/database/{{deploy_env}}/dump.sql');
-    }
+    run('mysqldump --host={{deploy_db_host}} --user={{deploy_db_user}} --password=\'{{deploy_db_pass}}\' {{deploy_db_name}} > {{deploy_path}}/{{deploy_env}}.sql');
+    download('{{deploy_path}}/{{deploy_env}}.sql', $_ENV['SUPERDOCK_PROJECT_DIR'] . '/superdock/database/{{deploy_env}}/dump.' . date("Y.m.d-H:i:s") . '.sql');
+    run('rm {{deploy_path}}/{{deploy_env}}.sql');
 });
--- src/Command/dumpCommand.php
+++ src/Command/dumpCommand.php
@@ -27,15 +27,32 @@ class dumpCommand extends Command
         
         envService::docker();
 
-        $cmd = [ 
-            $_ENV['SUPERDOCK_CORE_DIR'] . '/vendor/deployer/deployer/bin/dep', 
-            '--file=' . $_ENV['SUPERDOCK_CORE_DIR'] . '/inc/dep/dump.php', 
-            'dump',
-            $input->getArgument('env')
-        ];
-        if ( $input->getOption('verbose') ) array_push( $cmd, '-vvv' ); 
-        coreService::process($cmd);
-        $output->writeln( coreService::infos( 'The ' . $input->getArgument('env') . ' environement has been successfully saved in superdock/database/' . $input->getArgument('env') . '/dump.sql' ) );
+        if ( $input->getArgument('env') == 'local' ) {
+            
+            coreService::process([ 
+                'docker-compose', 
+                '-f' . $_ENV['SUPERDOCK_CORE_DIR'] . '/inc/docker/docker-compose.yml', 
+                'exec', 
+                'webserver', 
+                'sh', 
+                '-c', 
+                'mysqldump --host=superdock_database --user=root --password=root ' . $_ENV['SUPERDOCK_LOCAL_DB_NAME'] . ' > /var/www/html/superdock/database/local/dump.' . date("Y.m.d-H:i:s") . '.sql'
+            ]);
+        
+        } else {
+
+            $cmd = [ 
+                $_ENV['SUPERDOCK_CORE_DIR'] . '/vendor/deployer/deployer/bin/dep', 
+                '--file=' . $_ENV['SUPERDOCK_CORE_DIR'] . '/inc/dep/dump.php', 
+                'dump',
+                $input->getArgument('env')
+            ];
+            if ( $input->getOption('verbose') ) array_push( $cmd, '-vvv' ); 
+            coreService::process($cmd);
+
+        }
+
+        $output->writeln( coreService::infos( 'The ' . $input->getArgument('env') . ' environement has been successfully saved in superdock/database/' . $input->getArgument('env') . '/dump.{timestamp}.sql' ) );
 
         return Command::SUCCESS;
     }
